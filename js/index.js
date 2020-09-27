cc3fb65f1b330eb5795d6acedc874000779a846b
FrancineU <uwizeyimanafrancine62@gmail.com>
2020-09-27 23:33:31 +0200

    crate variable in js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-var 
+
+function validateForm(){
+    var cc =document.forms[myForm]["cc"];
+    var dd=document.forms[myForm]["date"];
+    var mm=document.forms[myForm]["month"];
+    var yy=document.forms[myForm]["year"];
+   var gender=document.forms[myForm]["gender"];
+if()
+}
